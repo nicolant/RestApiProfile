2f6207b5e92f68a7bc309aedc29768c1e459f24f
nicolant <nicolant@mail.ru>
2018-09-11 11:44:04 +0300

    Update api.php

--- templates/api.php
+++ templates/api.php
@@ -1,3 +1,8 @@
 <?php namespace ProcessWire;
 
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-PINGOTHER, Authorization, Post, Origin, X-Requested-With, Content-Type, Accept');
+
 require "{$config->paths->templates}api/Router.php";
